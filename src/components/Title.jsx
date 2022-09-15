b64a813d2031be6ddb4bc2da6bba7071be0c8a42
Vitosnatios <vitosdeveloper@gmail.com>
2022-09-15 02:53:42 -0300

    more style, less code, less userless files

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 export default function Title() {
   return (
-    <div className="title">
-      <h1>CHAT - REACT, NODE, EXPRESS, MYSQL.</h1>
+    <div className='title'>
+      <h1>✉ Chat by Vitos Developer - ReactJS, NodeJS, ExpressJS, MYSQL.</h1>
       <h3>
         Mensagens que ficarem pra trás das 3500 mais atuais serão deletadas da
         DB.
